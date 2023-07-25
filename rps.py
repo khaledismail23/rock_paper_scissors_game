1ab630818c024dfb48f320c94dafd9c2e7a0eff2
khaled ismail <khaledismail061@gmail.com>
2023-07-25 08:38:52 +0300

    Rock, Paper, Scissors Game

--- rps.py
+++ rps.py
@@ -3,13 +3,13 @@ import sys
 
 
 class Player:
-    moves = ['rock', 'paper', 'scissors']
+    moves = ["rock", "paper", "scissors"]
 
     def __init__(self):
         self.score = 0
 
     def move(self):
-        return 'rock'
+        return "rock"
 
     def learn(self, my_move, their_move):
         self.my_move = my_move
@@ -31,16 +31,19 @@ class HumanPlayer(Player):
     def move(self):
         while True:
             try:
-                move = input('Rock, paper, scissors? ')
+                move = input("Rock, paper, scissors? ").lower()
                 if move in self.moves:
                     return move
-                if move == 'quit':
+                if move == "quit":
                     sys.exit()
             except:
                 sys.exit()
 
 
 class ReflectPlayer(Player):
+    '''
+    ReflectPlayer class that remembers what move the opponent played last round, and plays that move this round.
+    '''
     def __init__(self):
         super().__init__()
         self.their_move = random.choice(self.moves)
@@ -50,9 +53,12 @@ class ReflectPlayer(Player):
 
 
 class CyclePlayer(Player):
+    '''
+    CyclePlayer class that remembers what move it played last round, and cycles through the different moves.
+    '''
     def __init__(self):
         super().__init__()
-        self.my_move = random.choice(self.moves)
+        self.my_move = self.moves[2]
 
     def move(self):
         if self.my_move == self.moves[0]:
@@ -69,9 +75,11 @@ class Game:
         self.p2 = p2
 
     def beats(self, one, two):
-        return ((one == 'rock' and two == 'scissors') or
-                (one == 'scissors' and two == 'paper') or
-                (one == 'paper' and two == 'rock'))
+        return (
+            (one == "rock" and two == "scissors")
+            or (one == "scissors" and two == "paper")
+            or (one == "paper" and two == "rock")
+        )
 
     def play_round(self):
         move1 = self.p1.move()
@@ -80,20 +88,20 @@ class Game:
         print(f"Opponent played {move2}.")
         if self.beats(move1, move2):
             self.p1.score += 1
-            print('** PLAYER ONE WINS **')
-            print(f'Score: Player One {self.p1.score},')
-            print(f'Player Two {self.p2.score}')
+            print("** PLAYER ONE WINS **")
+            print(f"Score: Player One {self.p1.score},")
+            print(f"Player Two {self.p2.score}")
         elif move1 == move2:
-            self.p1. score = self.p1.score
-            self.p2. score = self.p2.score
-            print('** TIE **')
-            print(f'Score: Player One {self.p1.score},')
-            print(f'Player Two {self.p2.score}')
+            self.p1.score = self.p1.score
+            self.p2.score = self.p2.score
+            print("** TIE **")
+            print(f"Score: Player One {self.p1.score},")
+            print(f"Player Two {self.p2.score}")
         else:
             self.p2.score += 1
-            print('** PLAYER TWO WINS **')
-            print(f'Score: Player One {self.p1.score},')
-            print(f'Player Two {self.p2.score}')
+            print("** PLAYER TWO WINS **")
+            print(f"Score: Player One {self.p1.score},")
+            print(f"Player Two {self.p2.score}")
 
         self.p1.learn(move1, move2)
         self.p2.learn(move2, move1)
@@ -101,8 +109,8 @@ class Game:
     def play_game(self):
         rounds = 5
         print("Game start!")
-        print(f'The Game is {rounds} rounds,')
-        print('player with the higher score is the winner')
+        print(f"The Game is {rounds} rounds,")
+        print("player with the higher score is the winner")
         print('to exit, type "quit" or press ctrl + c')
         print()
         for round in range(rounds):
@@ -114,11 +122,28 @@ class Game:
             print("Game over!, Player Two Won")
         else:
             print("It's a Tie")
-        print('final score:')
-        print(f'player one: {self.p1.score}')
-        print(f'player two: {self.p2.score}')
+        print("final score:")
+        print(f"player one: {self.p1.score}")
+        print(f"player two: {self.p2.score}")
 
 
-if __name__ == '__main__':
-    game = Game(HumanPlayer(), random.choice([Player(), RandomPlayer(), ReflectPlayer(), CyclePlayer()]))
+def chose_player():
+    strategies = {1: Player(), 2: RandomPlayer(),
+                  3: ReflectPlayer(), 4: CyclePlayer()}
+
+    while True:
+        try:
+            player = int(
+                input(
+                    "select the player you want to play against:\n1) Rock Player\n2) Random Player\n3) Reflect Player\n4) Cycle Player\n"
+                )
+            )
+            if player in [1, 2, 3, 4]:
+                return strategies[player]
+        except ValueError:
+            pass
+
+
+if __name__ == "__main__":
+    game = Game(HumanPlayer(), chose_player())
     game.play_game()
